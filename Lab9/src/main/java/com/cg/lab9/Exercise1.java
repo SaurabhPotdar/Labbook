35c18e9f54be780ff060313d306f5029033ca240
Saurabh Potdar <44492033+SaurabhPotdar@users.noreply.github.com>
2019-09-24 18:25:42 +0530

    Add files via upload
    Lab 9

---
+++ Lab9/src/main/java/com/cg/lab9/Exercise1.java
@@ -0,0 +1,18 @@
+package com.cg.lab9;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+
+public class Exercise1 {
+	
+	public static void main(String args[]) {
+		
+	}
+	
+	public List<Integer> getValues(HashMap<Integer,Integer> map){
+		
+		return null;
+	}
+
+}
